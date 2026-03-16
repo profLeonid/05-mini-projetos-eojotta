81c484298069e1e18e0439a70ca5be6b9814571a
JOÃO PEDRO DOS SANTOS ROMUALDO <25203692@CFP127ALUNO.LOCAL>
2026-03-16 17:02:22 -0300

    metade exercício animais pronto

---
+++ 05-escolha-Animal/app.js
@@ -0,0 +1,19 @@
+'use scrict'
+
+function definirEscolha(){
+    const escolha = getElementbyid('select').value
+    if(escolha == 'vertebrado'){
+        vertebrado()
+    
+    }else{
+        invertebrado()
+    }
+}
+
+function vertebrado(){
+
+}
+
+function invertebrado(){
+
+}
