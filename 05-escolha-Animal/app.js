2a340dce88d8a37415fb9d1ca3ccff45d25d91bf
JOÃO PEDRO DOS SANTOS ROMUALDO <25203692@CFP127ALUNO.LOCAL>
2026-03-23 15:33:47 -0300

    feito escolha animais

--- 05-escolha-Animal/app.js
+++ 05-escolha-Animal/app.js
@@ -1,19 +1,84 @@
 'use scrict'
 
-function definirEscolha(){
-    const escolha = getElementbyid('select').value
-    if(escolha == 'vertebrado'){
-        vertebrado()
+function atualizarSelect(){
+    const select1 = document.getElementById('select1').value
+    const select2 = document.getElementById('select2')
+    const select3  = document.getElementById('select3')
+
+    select2.innerHTML = '<option value =""></option>"'
+    select3.innerHTML = '<option value =""></option>"'
     
-    }else{
-        invertebrado()
+
+    if (select1 === 'vertebrado'){
+        select2.innerHTML += '<option value = "mamifero">Mamífero</option>'
+        select2.innerHTML += '<option value = "ave">Ave</option>'
+    }else if (select1 === 'invertebrado'){
+        select2.innerHTML += '<option value = "inseto">Inseto</option>'
+        select2.innerHTML += '<option value = "anelideo">Anelídeo</option>'
+    }
+
+}
+
+function atualizarSelect2(){
+    const select2 = document.getElementById('select2').value
+    const select3 = document.getElementById('select3')
+
+    select3.innerHTML = '<option value =""></option>"'
+
+   
+    if (select2 === 'mamifero'){
+    select3.innerHTML = '<option value        = "onivoro">Onívoro</option>'
+    select3.innerHTML += '<option value       = "herbivoro">Herbívoro</option>'
+
+    }else if(select2 === 'ave'){
+        select3.innerHTML += '<option value   = "carnivoro">Carnívoro</option>'
+        select3.innerHTML += '<option value   = "onivoro">Onívoro</option>'
+    
+    }else if(select2 === 'inseto'){
+        select3.innerHTML += '<option value   = "hematofago">Hematófago</option>'
+        select3.innerHTML += '<option value   = "herbivoro">Herbívoro</option>'
+    }else if(select2 === 'anelideo'){
+        select3.innerHTML += '<option value   = "hematofago">Hematófago</option>'
+        select3.innerHTML += '<option value   = "onivoro">Onívoro</option>'
     }
 }
 
-function vertebrado(){
+function definirEscolha(){
+   
+    const select2 = document.getElementById('select2').value
+    const select3 = document.getElementById('select3').value
+    const resultado = document.getElementById('resultado')
 
-}
+    if(select2 === 'mamifero'){
+        if(select3 === 'onivoro'){
+             resultado.textContent = 'Homem'
+        }else{
+            resultado.textContent = 'Vaca'
+        }
+    
+    
+    }else if(select2 === 'ave'){
+        if(select3 === 'carnivoro'){
+            resultado.textContent = 'Águia'
+        }else{
+            resultado.textContent = 'Pomba'
+        }
+   
+    
+    }else if (select2 === 'inseto'){
+        if(select3 === 'hematofago'){
+            resultado.textContent = 'pulga'
+        }else{
+            resultado.textContent = 'lagarta'
+        }
 
-function invertebrado(){
+    }else if (select2 === 'anelideo'){
+        if(select3 === 'hematofago'){
+            resultado.textContent = 'sanguessuga'
+        }else{
+            resultado.textContent = 'minhoca'
+        }
+    }
+
 
 }
